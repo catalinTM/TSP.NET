1b4deccbe4ef148c79bbc547efec54dac4379006
catalinTM <35262902+catalinTM@users.noreply.github.com>
2020-03-22 13:38:24 +0200

    Add files via upload

---
+++ Laborator4/Laborator4/Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Laborator4
+{
+    public partial class Customer
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Customer()
+        {
+            this.Orders = new HashSet<Order>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+
+    public partial class Order
+    {
+        public int OrderId { get; set; }
+        public decimal TotalValue { get; set; }
+        public System.DateTime Date { get; set; }
+        public int CustomerId { get; set; }
+
+        public virtual Customer Customer { get; set; }
+    }
+
+}
---
+++ Laborator4/Laborator4/ModelContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laborator4
+{
+    internal class ModelContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-HH6UKRQ;Database = EFCore2020; Trusted_Connection = True");
+
+        }
+    }
+}
