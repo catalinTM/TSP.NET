2200938b711893a0209e3673ff2f375179391c7a
catalinTM <35262902+catalinTM@users.noreply.github.com>
2020-03-22 12:05:20 +0200

    Add files via upload

---
+++ Laborator5/Laborator5/EF6RecipesContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator5
+{
+    class EF6RecipesContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public EF6RecipesContext() : base("Server = DESKTOP-HH6UKRQ;Database=Lab5; Trusted_Connection=True")
+        { }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Product>()
+            .Map(m =>
+            {
+                m.Properties(p => new { p.SKU, p.Description, p.Price });
+                m.ToTable("Product", "BazaDeDate");
+            })
+            .Map(m =>
+            {
+                m.Properties(p => new { p.SKU, p.ImageURL });
+                m.ToTable("ProductWebInfo", "BazaDeDate");
+            });
+        }
+    }
+}
