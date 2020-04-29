331d97eabee25c622e25c1e1d9240d6aef9e8b7b
catalinTM <35262902+catalinTM@users.noreply.github.com>
2020-04-29 14:02:33 +0300

    Add files via upload

---
+++ gRPC_si_EF/GrpcGreeterClient/GrpcGreeterClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GrpcGreeter;
+using Grpc.Net.Client;
+
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+
+            static async Task AddComment(GrpcChannel channel, Comment c)
+            {
+
+                var reply = await client.AddComment(c);
+                Console.WriteLine(reply.Result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
