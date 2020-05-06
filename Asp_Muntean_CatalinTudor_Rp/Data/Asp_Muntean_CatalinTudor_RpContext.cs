b75e35089ae19a1fba96404a94164e98a8a3129f
catalinTM <35262902+catalinTM@users.noreply.github.com>
2020-05-06 10:12:00 +0300

    Add files via upload

---
+++ Asp_Muntean_CatalinTudor_Rp/Data/Asp_Muntean_CatalinTudor_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Muntean_CatalinTudor_Rp.Models;
+
+namespace Asp_Muntean_CatalinTudor_Rp.Data
+{
+    public class Asp_Muntean_CatalinTudor_RpContext : DbContext
+    {
+        public Asp_Muntean_CatalinTudor_RpContext (DbContextOptions<Asp_Muntean_CatalinTudor_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Muntean_CatalinTudor_Rp.Models.Movie> Movie { get; set; }
+    }
+}
